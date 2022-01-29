0a9c2bb6a088eb06410408814f0fcf97c23b9019
zouhair <ettarak.zouhair@gmail.com>
2022-01-29 23:45:07 +0100

    Condition Logic if and else

--- vuejs-3-Socks/main.js
+++ vuejs-3-Socks/main.js
@@ -1,8 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: "socks",
+            product: " Socks",
             image: './assets/images/socks_blue.jpg',
+            InStock: true,
+            inventory: 0,
         }
     }
 }).mount('#app');
