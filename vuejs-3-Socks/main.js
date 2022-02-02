de9da37f165e2a0a0b4f04502ab481ff3cfe1c19
zouhair <ettarak.zouhair@gmail.com>
2022-02-02 20:05:47 +0100

    Boucle v-for

--- vuejs-3-Socks/main.js
+++ vuejs-3-Socks/main.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             InStock: true,
             inventory: 0,
-        }
+            details: ['50% Cotton','30% wool', '20% Polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ]
+        };
+
     }
 }).mount('#app');
