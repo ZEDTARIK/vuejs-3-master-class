c635e9ba839b5f1a133bb765f48c6ace2eda630b
zouhair <ettarak.zouhair@gmail.com>
2022-02-02 20:16:58 +0100

    Handle Event

--- vuejs-3-Socks/main.js
+++ vuejs-3-Socks/main.js
@@ -7,10 +7,19 @@ const app = Vue.createApp({
             inventory: 0,
             details: ['50% Cotton','30% wool', '20% Polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'}
-            ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            ],
+            cart:0,
         };
 
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
+        }
     }
 }).mount('#app');
